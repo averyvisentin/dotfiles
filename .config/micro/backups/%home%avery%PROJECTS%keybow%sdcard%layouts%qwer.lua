d530c69830c9a9de744a9da8c8754fa308dc1076
averyvisentin <averyvisentin@gmail.com>
2025-07-10 10:18:29 -0400

    removed avery- from keybinding and window rule conf, adjusted hyprland conf accordingly

---
+++ .config/micro/backups/%home%avery%PROJECTS%keybow%sdcard%layouts%qwer.lua
@@ -0,0 +1,51 @@
+require "keybow"
+
+-- Key mappings --
+
+function handle_key_00(pressed)
+    keybow.set_key(keybow.F4, pressed)
+end
+
+function handle_key_01(pressed)
+    keybow.set_key(keybow.F8, pressed)
+end
+
+function handle_key_02(pressed)
+    keybow.set_key(keybow.F12, pressed)
+end
+
+function handle_key_03(pressed)
+    keybow.set_key(keybow.F3, pressed)
+end
+
+function handle_key_04(pressed)
+    keybow.set_key(keybow.F7, pressed)
+end
+
+function handle_key_05(pressed)
+    keybow.set_key(keybow.F11, pressed)
+end
+
+function handle_key_06(pressed)
+    keybow.set_key(keybow.F2, pressed)
+end
+
+function handle_key_07(pressed)
+    keybow.set_key(keybow.F6, pressed)
+end
+
+function handle_key_08(pressed)
+    keybow.set_key(keybow.F10, pressed)
+end
+
+function handle_key_09(pressed)
+    keybow.set_key(keybow.Z, pressed)
+end
+
+function handle_key_10(pressed)
+    keybow.set_key(keybow.F5, pressed)
+end
+
+function handle_key_11(pressed)
+    keybow.set_key(keybow.F9, pressed)
+end
